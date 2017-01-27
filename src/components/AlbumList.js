7014d95f67e529ddeea43ed79322e753548d9c77
Antonella Solomon <antonellasolomon@gmail.com>
2017-01-27 16:35:44 -0500

    fetching data from test api

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -3,10 +3,23 @@ import { View, Text } from 'react-native';
 
 class AlbumList extends Component {
 
+  state = { albums: [] };
+  componentWillMount() {
+    fetch('https://rallycoding.herokuapp.com/api/music_albums')
+    .then((response) => response.json())
+    .then((responseData) => this.setState({ albums: responseData }));
+
+  }
+
+  renderAlbums() {
+    return this.state.albums.map(album => <Text>{album.title}</Text>);
+  }
+
   render() {
+    console.log(this.state);
   return (
     <View>
-      <Text>Album List!!!</Text>
+      {this.renderAlbums()}
     </View>
     );
   }
