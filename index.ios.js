48b82047f6c8ca9c24d3793f0ce01db0a838ef77
Antonella Solomon <antonellasolomon@gmail.com>
2017-01-27 18:06:59 -0500

    added some styling elements

--- index.ios.js
+++ index.ios.js
@@ -2,7 +2,7 @@
 
 // import library to help create a component
 import React from 'react';
-import { AppRegistry, View} from 'react-native';
+import { AppRegistry, View } from 'react-native';
 import Header from './src/components/Header';
 import AlbumList from './src/components/AlbumList'
 
--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -4,14 +4,17 @@ import AlbumDetail from './AlbumDetail';
 
 class AlbumList extends Component {
 
+// initializing state to empty
   state = { albums: [] };
+
+  // fetching album content from test api
   componentWillMount() {
     fetch('https://rallycoding.herokuapp.com/api/music_albums')
     .then((response) => response.json())
     .then((responseData) => this.setState({ albums: responseData }));
 
   }
-
+// provide album detail
   renderAlbums() {
     return this.state.albums.map(album =>
       <AlbumDetail key={album.title} album={album} />
