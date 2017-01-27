825a6ce4a0a608775d9d29c1845ad4d0f27e713f
Antonella Solomon <antonellasolomon@gmail.com>
2017-01-27 18:18:38 -0500

    added scrollview

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -4,15 +4,17 @@ import Card from './Card';
 import CardSection from './CardSection';
 
 const AlbumDetail = ({ album }) => {
-  const { title, artist, thumbnail_image } = album;
+  const { title, artist, thumbnail_image, image } = album;
   const { thumbnailStyle,
     headerContentStyle,
     thumbnailContainerStyle,
-    headerTextStyle } = styles;
+    headerTextStyle,
+    imageStyle
+  } = styles;
 return (
     <Card>
       <CardSection>
-      <View>
+      <View style={thumbnailContainerStyle}>
       <Image
         style={thumbnailStyle}
         source={{ uri: thumbnail_image }} />
@@ -21,6 +23,11 @@ return (
         <Text style={headerTextStyle}>{title}</Text>
         <Text>{artist}</Text>
         </View>
+        </CardSection>
+        <CardSection>
+          <Image
+          style={imageStyle}
+          source={{ uri: image }} />
         </CardSection>
     </Card>
   );
@@ -43,6 +50,11 @@ const styles = {
     alignItems: 'center',
     marginLeft: 10,
     marginRight: 10
+  },
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: null
   }
 };
 
